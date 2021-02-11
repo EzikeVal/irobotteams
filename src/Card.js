90846ad52a3d4ef83f47d923737275084b1972fc
ezikechris <christophermbe1009@gmail.com>
2021-02-11 17:40:32 +0100

    First Commit

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Card = () => {
+    return (
+        <div>
+            <img src='' alt='' />
+            <div>
+                <h2> Jane Doe</h2>
+                <p>Jane.doe@gmail.com</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Card;
